c532f89bcfd4f5fae81bdc9372e8bb15f64e9a7e
ericlee215 <51139205+ericlee215@users.noreply.github.com>
2023-06-20 16:19:45 -0700

    Update We_to_Re.m

--- src/We_to_Re.m
+++ src/We_to_Re.m
@@ -1,9 +1,26 @@
-function [Re, Oh, V] = We_to_Re(We, rho, mu, sigma, D)
+function [Re, Oh, V] = We_to_Re(d,f)
+%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%
+% Inputs  Matlab Struct, "d", short for "drop" contains:
+%                 Drop impact Weber number, "We"
+%                 Drop initial radius, "r0"
+%         Matlab Struct, "f", short for "fluid" contains:
+%                 Fluid density, "rho"
+%                 Fluid kinematic viscocity, "nu"
+%                 Fluid dynamic viscocity, "mu"
+%                 Fluid surface tension, "sigma
+%
+%       (Verify consistent Units)
+%                 
+% Returns Impact Reynolds number, "Re" 
+%         Impact Ohnesorge number, "Oh"
+%         Impact Velocity, "V"
+%
+%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%
 
-V = sqrt(We*sigma/(rho*D));
+V = sqrt(d.We*f.sigma/(f.rho*2*d.r0));
 
-Re = rho*V*D/mu;
+Re = f.rho*V*2*d.r0/f.mu;
 
-Oh = sqrt(We)/Re;
+Oh = sqrt(d.We)/Re;
 
 end
